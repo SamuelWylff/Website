287fb377372ff33259e29baa4501fa1b7a97a9b2
Samuel Wylff <38791989+SamuelWylff@users.noreply.github.com>
2018-12-01 11:02:31 +0000

    Add files via upload

---
+++ Web-development/index.php
@@ -0,0 +1,91 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="sources/styles.css">
+        <title>Samuel-R. Wylff</title>
+    </head>
+    <body>
+        <?php include '.sources/modules/theme.php';?>
+        <div class="wrapper">
+        <br>
+            <div class="box medium">
+                <h1>About me.</h1>
+                <hr>
+                <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                <div class="columns">
+                    <div class="column">
+                            <div class="box">
+                                    <h1>About me.</h1>
+                                    <hr>
+                                    <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                            </div>
+                    </div>
+                    <div class="column">
+                            <div class="box">
+                                    <h1>About me.</h1>
+                                    <hr>
+                                    <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                            </div>
+                    </div>
+                    <div class="column">
+                            <div class="box">
+                                    <h1>About me.</h1>
+                                    <hr>
+                                    <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="columns">
+                        <div class="column">
+                                <div class="box">
+                                        <h1>About me.</h1>
+                                        <hr>
+                                        <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                                </div>
+                        </div>
+                        <div class="column">
+                                <div class="box">
+                                        <h1>About me.</h1>
+                                        <hr>
+                                        <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                                </div>
+                        </div>
+                        <div class="column">
+                                <div class="box">
+                                        <h1>About me.</h1>
+                                        <hr>
+                                        <p>I am a fifteen-year-old male from North-Western England. I was born and raised in the Greater Manchester Area.</p>
+                                </div>
+                        </div>
+                    </div>
+                <button>This is a test button!</button>
+            </div>
+            <div class="box medium">
+                <h1>Current Projects</h1>
+                <hr>
+                <p>My current projects include:</p>
+                <ul class="b">
+                    <li class="b">
+                            Project Wylff, a CSS Framework project.
+                    </li>
+                    <li class="b">
+                            Project Utonagan, a Windows PE-based Windows installation environment.
+                    </li>
+                    <li class="b">
+                            Project Kugsha, a custom Windows 10 SKU (incl. Project Untonagan).
+                    </li>
+                </ul>
+            </div>
+        <br>
+        </div>    
+
+        <div class="footer">
+                <br>
+                <p>Website (incl. HTML and CSS) made by <b>Samuel-R. Wylff</b>.</p>
+                <br>
+                <p>Built courtesy of the <a href="#wylff" target="_blank">Wylff Framework.</a></p>
+                <br>
+                <p>All rights reserved.</p>
+                <br>
+        </div>
+    </body>
+</html>
---
+++ Web-development/sources/modules/theme.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <div class="theme">
+                <div class="navbar">
+                        <a href="index.html"><b>Samuel Wylff</b></a>
+                        <a href="wylffsoft.html">Wylff Software</a>
+                </div>
+                <div class="banner">
+                        <p style="font-size:200%;"><b>Samuel-R. Wylff</b></p>
+                        <p>"I'd rather die."</p>
+                </div>
+        </div>
+    </body>
+</html>

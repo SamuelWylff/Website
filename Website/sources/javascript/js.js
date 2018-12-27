6d5b14a2dd3afd04ac51b3d2aba0fe70f184c657
Samuel Wylff <38791989+SamuelWylff@users.noreply.github.com>
2018-12-27 12:08:53 +0000

    Delete js.js

--- Website/sources/javascript/js.js
+++
@@ -1,8 +0,0 @@
-function hideCookies() {
-    var x = document.getElementById("coookiebox").innerHTML;
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-}
